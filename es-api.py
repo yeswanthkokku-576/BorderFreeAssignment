01f6bea0acee9724d26596b57ef50b551d4d3b9b
yeswanthkokku-576 <71124834+yeswanthkokku-576@users.noreply.github.com>
2021-04-19 15:00:57 +0530

    Update es-api.py

--- es-api.py
+++ es-api.py
@@ -5,14 +5,14 @@ import json
 import os
 
 def lambda_handler(event, context):
-    region = 'us-east-1'
+    region_name = 'us-east-1'
     service = 'es'
-    host='https://search-changedata-lq2yok2wmkhwzeu7awauv4a3km.us-east-1.es.amazonaws.com'
-    index = 'data'
+    hostname='https://search-changedata-lq2yok2wmkhwzeu7awauv4a3km.us-east-1.es.amazonaws.com'
+    indexname = 'data'
     type = 'lambda-type'
     credentials = boto3.Session().get_credentials()
-    awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
-    url = host + '/' + index + '/_search/?size=300'
+    awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region_name, service, session_token=credentials.token)
+    url = hostname + '/' + indexname + '/_search/?size=300'
     headers = {"Content-Type": "application/json"}
     #getting the data from elasticsearch using host and indexname
     response = requests.get(url, auth=awsauth, headers=headers)
