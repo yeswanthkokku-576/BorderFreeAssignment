69c7701a57ac15dea4f572ba9671f3118640a960
yeswanthkokku-576 <71124834+yeswanthkokku-576@users.noreply.github.com>
2021-04-23 00:12:20 +0530

    Update insert.py

--- insert.py
+++ insert.py
@@ -1,35 +1,36 @@
+import boto3
 import csv
 import os
 def lambda_handler(event, context):
-    region_name='us-east-1'
-    filename='cupcake.csv'
+    region='us-east-1'
+    file_name='cupcake.csv'
     bucket_name='borderfree1'
     table_name='cakedata'
     records=[]
     s3=boto3.client('s3')            
-    dynamodb_client = boto3.client('dynamodb', region_name=region_name)
-    s3_object= s3.get_object(Bucket=bucket_name, Key=filename)
+    dynamodb = boto3.resource('dynamodb',region_name = 'us-east-1')
+    table = dynamodb.Table('cakedata')
+    s3_object= s3.get_object(Bucket=bucket_name, Key=file_name)
+    print(type(s3_object))
     records = s3_object['Body'].read().decode('utf-8').split('\n')
     count=0
     for i in range(len(records)-1):
-        if (i==0):
-            continue
         data=records[i]
         count=count+1
         data_list=data.split(",")
+        print(data_list[1])
         Month=data_list[0]
         cupcake=data_list[1].replace('\r', '')
-        
-        
-    #inserting data to dynamodb by using put item method
-        response = dynamodb_client.put_item(
-            TableName=table_name,
+        #inserting data to dynamodb using boto3 put_item method
+        #print(Month)
+    
+        response = table.put_item(
             Item={
-            'id' :{'S':str(count)},
-            'Month' : {'S':Month},
-            'cupcake': {'S':cupcake},
-            
+            'id' :str(count),
+            'Month' :Month,
+            'cupcake':cupcake,
             }
+            
         )
     
     print('Values Inserted To DynamoDB:')
