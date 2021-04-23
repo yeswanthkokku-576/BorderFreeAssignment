dc65b86999c8cfb58d98a3ae03634838358a6c8c
yeswanthkokku-576 <71124834+yeswanthkokku-576@users.noreply.github.com>
2021-04-23 13:39:32 +0530

    Update update.py

--- update.py
+++ update.py
@@ -5,13 +5,12 @@ import datetime
 import os
 
 def lambda_handler(event, context):
-    dynamodb = boto3.resource('dynamodb')
+    dynamodb = boto3.resource('dynamodb',region_name = 'us-east-1')
     table = dynamodb.Table('cakedata')
-    dyndb_client = boto3.client('dynamodb', 'us-east-1')
-    #Generates Random values for id
     for i in range(100):
         x=random.randrange(1, 207)
-        #Get values from dynamodb using above generated randomly generate id
+
+        #Get values from dynamodb using above generated random primarkey--id
         response = table.get_item(
                     Key={
                         'id': str(x),
@@ -21,12 +20,13 @@ def lambda_handler(event, context):
         print(x)
         updated_cupcake_value=random.randrange(1,101)
         item = response['Item']
-        #update new column in dynamodb using put_item method
-        response=dyndb_client.put_item(
-            TableName='cakedata',
+        
+        response=table.put_item(
+            
             Item={
-                    'id' : {'S':item['id']},
-                    'Month': {'S':item['Month']},
-                    'cupcake': {'S':str(updated_cupcake_value)},
-                    'updatetime':{'S':str( datetime.datetime.now())},
+                    'id' : item['id'],
+                    'Month': item['Month'],
+                    'cupcake': str(updated_cupcake_value),
+                    'updatetime':str( datetime.datetime.now()),
             })
+        #print(response)
